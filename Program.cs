db3ef9efce6b62d99ccf136123ac3941585d282b
Slay Stina <august.hogbom@gmail.com>
2024-10-17 11:04:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TjuvochPolis_Grupp_H
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
